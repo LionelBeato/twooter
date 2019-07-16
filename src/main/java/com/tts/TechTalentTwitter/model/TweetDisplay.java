ef6b3fc5f58e5e95061453f6fc91fb5dbaa074bb
LionelBeato <lionel.beato@gmail.com>
2019-07-16 12:58:17 -0400

    finished last slides

---
+++ src/main/java/com/tts/TechTalentTwitter/model/TweetDisplay.java
@@ -0,0 +1,34 @@
+package com.tts.TechTalentTwitter.model;
+
+import java.util.List;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TweetDisplay {
+    private User user;	
+    private String message;
+    private String date;
+    private List<Tag> tags;
+}
